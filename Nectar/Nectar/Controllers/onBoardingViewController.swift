1819c30c23c9474b8c78bca42bbfab6b44f08886
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-10-10 23:44:13 +0100

    Implemented the launch screen

--- Nectar/Nectar/Controllers/onBoardingViewController.swift
+++ Nectar/Nectar/Controllers/onBoardingViewController.swift
@@ -11,11 +11,18 @@ class onBoardingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        assignbackground()
+        assignBackground()
+        for family: String in UIFont.familyNames
+                       {
+                           print(family)
+                           for names: String in UIFont.fontNames(forFamilyName: family)
+                           {
+                               print("== \(names)")
+                           }
+                       }
     }
-
-    func assignbackground(){
-            
+// MARK: - Function...Assigning an imageview to the background
+    func assignBackground(){
             var imageView : UIImageView!
             imageView = UIImageView(frame: view.bounds)
             imageView.contentMode = .scaleAspectFill
---
+++ Nectar/Nectar/Resources/Extensions.swift
@@ -0,0 +1,28 @@
+//
+//  Extensions.swift
+//  Nectar
+//
+//  Created by Decagon Macbook Pro on 10/10/2021.
+//
+
+import UIKit
+
+enum AppColors {
+    case white
+    case green
+    case lightBlue
+    case darkBlue
+    
+    var color: UIColor {
+        switch self {
+        case .white:
+            return UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        case .green:
+            return UIColor(red: 0.325, green: 0.694, blue: 0.459, alpha: 1)
+        case .lightBlue:
+            return UIColor(red: 0.325, green: 0.514, blue: 0.925, alpha: 1)
+        case .darkBlue:
+            return UIColor(red: 0.29, green: 0.4, blue: 0.675, alpha: 1)
+        }
+    }
+}
