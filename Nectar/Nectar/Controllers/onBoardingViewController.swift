98fc19e6074cc22b6c326114911fee026d071cdf
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-10-11 00:57:48 +0100

    Added "Get started" button to the onboarding Screen

--- Nectar/Nectar/Controllers/onBoardingViewController.swift
+++ Nectar/Nectar/Controllers/onBoardingViewController.swift
@@ -8,30 +8,45 @@
 import UIKit
 
 class onBoardingViewController: UIViewController {
+    
+    lazy var background: UIImageView = {
+        var imageView = UIImageView()
+        imageView.frame = view.bounds
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "OnBoarding")
+        imageView.center = view.center
+        return imageView
+    }()
 
+    lazy var getStartedButton: UIButton = {
+        let button = UIButton.makeCorneredButton()
+        button.titleLabel?.font = UIFont(name: AppFonts.gilroySemiBold.font, size: 18)
+        button.setTitle("Get Started", for: .normal)
+        button.backgroundColor = AppColors.green.color
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        assignBackground()
-        for family: String in UIFont.familyNames
-                       {
-                           print(family)
-                           for names: String in UIFont.fontNames(forFamilyName: family)
-                           {
-                               print("== \(names)")
-                           }
-                       }
+        view.backgroundColor = .white
+        addSubviews()
+        constraintViews()
     }
-// MARK: - Function...Assigning an imageview to the background
-    func assignBackground(){
-            var imageView : UIImageView!
-            imageView = UIImageView(frame: view.bounds)
-            imageView.contentMode = .scaleAspectFill
-            imageView.clipsToBounds = true
-            imageView.image = UIImage(named: "OnBoarding")
-            imageView.center = view.center
-            view.addSubview(imageView)
-            self.view.sendSubviewToBack(imageView)
-        }
-
+    
+    // MARK: - Function...Adding subviews
+    func addSubviews() {
+        view.addSubview(background)
+        view.addSubview(getStartedButton)
+    }
+    
+    // MARK: - Function...Adding constraints to subviews
+    func constraintViews() {
+        NSLayoutConstraint.activate([
+            getStartedButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -90.84),
+            getStartedButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30.5),
+            getStartedButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30.5),
+        ])
+   }
 }
 
--- Nectar/Nectar/Resources/Extensions.swift
+++ Nectar/Nectar/Resources/Extensions.swift
@@ -7,22 +7,12 @@
 
 import UIKit
 
-enum AppColors {
-    case white
-    case green
-    case lightBlue
-    case darkBlue
-    
-    var color: UIColor {
-        switch self {
-        case .white:
-            return UIColor(red: 0, green: 0, blue: 0, alpha: 0)
-        case .green:
-            return UIColor(red: 0.325, green: 0.694, blue: 0.459, alpha: 1)
-        case .lightBlue:
-            return UIColor(red: 0.325, green: 0.514, blue: 0.925, alpha: 1)
-        case .darkBlue:
-            return UIColor(red: 0.29, green: 0.4, blue: 0.675, alpha: 1)
-        }
+extension UIButton {
+    class func makeCorneredButton() -> UIButton {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 67).isActive = true
+        button.layer.cornerRadius = 19
+        return button
     }
 }
