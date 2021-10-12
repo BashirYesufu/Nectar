0a9c746dc5eb7bd068cbaddba21469d16ab34902
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-10-12 02:10:10 +0100

    Implemented the Welcome screen

--- Nectar/Nectar/Controllers/onBoardingViewController.swift
+++ Nectar/Nectar/Controllers/onBoardingViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class OnBoardingViewController: UIViewController {
+
+    lazy var logo: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.frame = CGRect(x: 0, y: 0, width: 48.47, height: 56.36)
+        imageView.image = UIImage(named: "Nectar Logo")
+        return imageView
+    }()
     
     lazy var background: UIImageView = {
         var imageView = UIImageView()
@@ -17,6 +25,26 @@ class OnBoardingViewController: UIViewController {
         imageView.image = UIImage(named: "OnBoarding")
         imageView.center = view.center
         return imageView
+    }()
+
+    lazy var welcomeLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome \nto our store"
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.font = UIFont(name: AppFonts.gilroySemiBold.font, size: 48)
+        label.textColor = AppColors.white.color
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Get your groceries in as fast as one hour"
+        label.font = UIFont(name: AppFonts.gilroyMedium.font, size: 16)
+        label.textColor = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 0.7)
+        return label
     }()
 
     lazy var getStartedButton: UIButton = {
@@ -44,6 +72,9 @@ class OnBoardingViewController: UIViewController {
         view.backgroundColor = AppColors.white.color
         view.addSubview(background)
         view.addSubview(getStartedButton)
+        view.addSubview(welcomeLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(logo)
     }
     
     // MARK: - Function...Adding constraints to subviews
@@ -52,6 +83,12 @@ class OnBoardingViewController: UIViewController {
             getStartedButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -90.84),
             getStartedButton.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 30.5),
             getStartedButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -30.5),
+            descriptionLabel.bottomAnchor.constraint(equalTo: getStartedButton.topAnchor, constant: -40.88),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            welcomeLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -19),
+            welcomeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logo.bottomAnchor.constraint(equalTo: welcomeLabel.topAnchor, constant: -35.66),
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
    }
 }
